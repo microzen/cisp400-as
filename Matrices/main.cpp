d239ab9004c44b5d608e1c19e84f3806bfc26350
Microzenas <microzenas@gmail.com>
2023-02-23 23:16:24 -0800

    M04

--- Matrices/main.cpp
+++ Matrices/main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
 #include "Matrices.h"
 int main(){
-    std::cout << "test";
-    return 0;
+    Matrices::Matrix a(2, 2);
+    Matrices::Matrix b(2, 4);
+
+    a(0, 0) = 0;
+    a(0, 1) = -1;
+    a(1, 0) = 1;
+    a(1, 1) = 0;
+
+    cout << "a:" << endl << a << endl;
+
+    b(0, 0) = 1;
+    b(0, 1) = 0.866025;
+    b(0, 2) = 1;
+    b(0, 3) = 0.5;
+    b(1, 0) = 0;
+    b(1, 1) = 0.5;
+    b(1, 2) = 1;
+    b(1, 3) = 0.866025;
+
+    cout << "\nb:" << endl << b << endl ;
+
+    Matrices::Matrix c(2, 4);
+    c = b + b;
+    cout << "\nc = b + b:"<< endl << c << endl;
+
+    c = a * b;
+    cout<< "\nc = a * b:" <<endl<< c << endl;
 }
